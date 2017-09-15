b4264329065298243bc4f64d6fe1a98d9308257a
CarolineXWL89 <carolinexwliu89@hotmail.com>
2017-09-15 13:20:51 -0700

    #1 Wired and string resources

--- app/src/main/java/com/example/caroline/quiz/MainActivity.java
+++ app/src/main/java/com/example/caroline/quiz/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.caroline.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView question;
+    private Button trueAns, falseAns, nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wireWidgets();
+    }
+
+    private void wireWidgets() {
+        trueAns = (Button) findViewById(R.id.button_true);
+        falseAns = (Button) findViewById(R.id.button_false);
+        nextButton = (Button) findViewById(R.id.button_next_question);
+        question = (TextView) findViewById(R.id.text_view_text_question);
     }
 }
---
+++ app/src/main/java/com/example/caroline/quiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.caroline.quiz;
+
+/**
+ * Created by princ on 15/09/2017.
+ */
+
+public class Question {
+    private String question;
+    private boolean answer;
+
+    public Question(String question, boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    //write the check answer method that returns whether or not the supplied boolean answer matches the real answer
+    /**
+     * @param userAnswer what the user selected
+     * @return true if answers match, false otherwise
+     */
+    public boolean checkAnswer(boolean userAnswer){
+        return answer == userAnswer;
+    }
+}
